2d17c9b547b63f3acbfa884576bc0b8f22cef360
Nick Eoneof <eoneof@gmail.com>
2022-05-14 19:27:24 +0300

    feat: add data simulation

---
+++ assets/simulateData.js
@@ -0,0 +1,15 @@
+﻿function simulateData() {
+  var _inks = {
+    'Process Cyan': {
+      angle: 75,
+    },
+    'Process Magenta': {
+      angle: 45,
+    },
+  };
+
+  var _docTextVar = {
+    name,
+    content,
+  };
+}
